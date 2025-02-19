cb9d68e892aa7c6e30c2ceffffc83d3bef59297d
Yernar17 <ernarmynzhasar8@gmail.com>
2025-02-19 22:58:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+// const hudies = [
+//     {
+//         Image: 'https://resources.cdn-kaspi.kz/img/m/p/h1c/h66/85514610573342.jpg?format=gallery-large',
+//         hudi: 'Без Надписи',
+//         money: '15 000'
+//     },
+//     {
+//         Image: 'https://www.cherokeeprint.com.ua/wp-content/uploads/2024/04/5358579216_tolstovka-s-printom.png',
+//         hudi: 'С рисунком',
+//         money: '17 000'
+//     },
+//     {
+//         Image: 'https://clans.by/assets/images/goods/polar-skate-co/default-hoodie-black-1_544x544.jpg',
+//         hudi: 'С надписью',
+//         money: '16 000'
+//     }
+// ]
+
+// const fubolki = [
+//     {
+//         Image: 'https://img.joomcdn.net/13b14a3b071658ee5b748e0d0da182a75b867762_original.jpeg',
+//         style: 'Без надписью',
+//         money: '5590'
+//     },
+//     {
+//         Image: 'https://image-thumbs.shafastatic.net/-279217970_310_430',
+//         style: 'Gym Hub',
+//         money: '6000'
+//     },
+//     {
+//         Image: 'https://ae04.alicdn.com/kf/S06f8e12fe64a4376b3f0f4aa43cd1b84U.jpg_480x480.jpg',
+//         style: 'Spider',
+//         money: '6000'
+//     }
+// ]
+
+// const triko = [
+//     {
+//         Image: 'https://ae04.alicdn.com/kf/S5bd089e42a9d460c9de3899938ea9fe5O.jpg_480x480.jpg',
+//         tur: 'Простой',
+//         money: '10 000'
+//     },
+//     {
+//         Image: 'https://ae01.alicdn.com/kf/S7351e16cd65449a988af790abf3ba013q.jpg',
+//         tur: 'Широки',
+//         money: '12 000'
+//     }
+// ]
+
+// const noski = [
+//     {
+//         Image: 'https://ir-3.ozone.ru/s3/multimedia-3/c1000/6405577299.jpg',
+//         nos: 'Белые Nike',
+//         money: '600'
+//     },
+//     {
+//         Image: 'https://frankfurt.apollo.olxcdn.com/v1/files/5v5gwsyxbw6j3-KZ/image;s=736x980',
+//         nos: 'Тренеровочные носки',
+//         money: '1000'
+//     }
+// ]
+
+// const aksessy = [
+//     {
+//         Image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlC_mHGkQjDu8-XMbuKckxGGX02bXp_9TVBA&s',
+//         aks: 'Резина',
+//         money: '2000'
+//     },
+//     {
+//         Image: 'https://lifehacker.ru/wp-content/uploads/2020/01/1036624662_1578573734.jpg',
+//         aks: 'Резина для ног',
+//         money: '1500'
+//     },
+//     {
+//         Image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk5wV1jc2q-bItQa1fYMRzO7jYJfCRkux2Sg&s',
+//         aks: 'Утижелитель',
+//         money: '3000'
+//     },
+//     {
+//         Image: 'https://ekb.uteka.ru/media/1024/2/b0/2b03fa1c7274d58e4576dfbeff9f9a91.jpg',
+//         aks: 'Тейп',
+//         money: '800'
+//     },
+//     {
+//         Image: 'https://bitrix.aptekaplus.kz/cdn/?q=/upload/iblock/b8f/biolabioplataxleykoplastyrnanetkanoyosnove25sm5m.jpg',
+//         aks: 'Тейп Bioplatex',
+//         money: '500'
+//     },
+//     {
+//         Image: 'https://kursor-shop.kz/image/cache/catalog/apple/airpods-pro2/type-c/type-c_1-600x600.jpg',
+//         aks: 'AirPods',
+//         money: '4500'
+//     }
+// ]
+
+// const short = [
+//     {
+//         Image: 'https://fanshop.by/upload/products/1243/orig/c825ebc5a4dce986ad715a7a29186807.jpg',
+//         shot: 'Nike черные',
+//         money: '5000'
+//     },
+//     {
+//         Image: 'https://limpopo.kz/image/cache/catalog/images_products/147132/shorty-sportivnye-muzhskie-scott-endurance-tech-hybrid-4-1000x1000.jpg.webp',
+//         shot: 'Шорты с внутренними шортами',
+//         money: '5500'
+//     }
+// ]
